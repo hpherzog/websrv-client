215333d1f8f73912a78b995231f6ca605358d88f
Hans-Peter Herzog <hans-peter.herzog@cdn-hph.de>
2014-07-29 21:39:39 +0200

    Initial

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var wrap = require('gulp-wrap');
 
+
 gulp.task('default', function() {
 
     var wrapTemplate = '(function(){<%= contents %>})();';
@@ -14,5 +15,14 @@ gulp.task('default', function() {
         .pipe(uglify())
         .pipe(wrap(wrapTemplate))
         .pipe(rename('websrv.min.js'))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.'))
+        .pipe(gulp.dest('../websrv/examples/default/public/js'))
+});
+
+gulp.task('watch', function(){
+
+    var watcher = gulp.watch('./src/**/*.js', ['default']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type);
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,53 +1,8 @@
 
-var console = require('console');
-var util = require('util');
-var events = require('events');
+var client = require('./client');
 
-function Client(options) {
+var websrv = {};
 
-    this.url = options.url;
+websrv.client = client;
 
-    this.webSocket = null;
-};
-
-Client.prototype.open = function open() {
-
-    if(this.webSocket === null && this.webSocket.readyState === 3) {
-        this.webSocket = new WebSocket(this.url);
-        this.webSocket.onopen = this.getOpenHandler();
-        this.webSocket.onclose = this.getCloseHandler();
-        this.webSocket.onerror = this.getErrorHandler();
-        this.webSocket.onmessage = this.getMessageHandler();
-    }
-};
-
-Client.prototype.getOpenHandler = function getOpenHandler() {
-
-    return function() {
-        console.log('Client opened');
-    };
-};
-
-Client.prototype.getCloseHandler = function getCloseHandler() {
-
-    return function() {
-        console.log('Client closed');
-    }
-};
-
-Client.prototype.getErrorHandler = function getErrorHandler() {
-
-    return function(err) {
-        console.error(err);
-    }
-};
-
-Client.prototype.getMessageHandler = function getMessageHandler() {
-
-    return function(message) {
-        console.error(err);
-    }
-};
-
-util.inherits(Client,
-    events.EventEmitter);
+window.websrv = websrv;
